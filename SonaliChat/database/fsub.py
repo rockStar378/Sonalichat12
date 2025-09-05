149cf3a24990284b7377631e7aea9c90da61a97c
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-09-05 20:17:24 +0530

    Update fsub.py

--- SonaliChat/database/fsub.py
+++ SonaliChat/database/fsub.py
@@ -1,38 +1,22 @@
-from pyrogram.errors import UserNotParticipant, ChannelInvalid
+from pyrogram.errors import UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import AUTH_CHANNEL
 
-
 async def get_fsub(bot, message):
-    target_channel_id = AUTH_CHANNEL  # Yeh config.py se aa raha hai
+    target_channel_id = AUTH_CHANNEL  
     user_id = message.from_user.id
-
     try:
-        # User channel 
         await bot.get_chat_member(target_channel_id, user_id)
-        return True
-
     except UserNotParticipant:
-    
-        try:
-            channel = await bot.get_chat(target_channel_id)
-            
-            channel_link = channel.invite_link or f"https://t.me/{channel.username}"
-        except Exception as e:
-            print(f"⚠️ Error fetching channel info: {e}")
-            return False
-
+        channel_link = (await bot.get_chat(target_channel_id)).invite_link
         join_button = InlineKeyboardButton("ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=channel_link)
-        keyboard = [[join_button]]
 
+        keyboard = [[join_button]]
         await message.reply(
             f"<b>🙌 Hey {message.from_user.mention()}, You're Almost There.</b>\n\n"
-            "<i>💡 Unlock the magic by joining our channel! Don't miss out on the fun and learning 🎉</i>",
+             "<i>💡 Unlock the magic by joining our channel! Don't miss out on the fun and learning 🎉</i>",
             reply_markup=InlineKeyboardMarkup(keyboard),
         )
         return False
-
-    except ChannelInvalid:
-        # Agar AUTH_CHANNEL galat hai
-        print(f"⚠️ Invalid AUTH_CHANNEL in config: {target_channel_id}")
-        return False
+    else:
+        return True
